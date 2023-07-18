95e46020da545546ab4b92dd2e4e5f76de910e7b
BrianHebert <butterflyrbrian40@gmail.com>
2023-07-18 09:54:47 -0500

    the recipe data shows up when clicking the food and when hovering the food the name shows up

--- src/components/RecipePictures.jsx
+++ src/components/RecipePictures.jsx
@@ -1,18 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components";
 import { NavLink } from "react-router-dom"
 
 const ImgStyling = styled.ul`
     margin: 10px;
     padding: 10px;
+    img{
+        width:400px;
+    }
 `;
 
 export default function RecipePictures(props){
+    const [isHovered, setIsHovered] = React.useState(false)
+    function handleHover(){
+        setIsHovered(true)
+    }
+    function handleUnhover(){
+        setIsHovered(false)
+    }
     return(
         <ImgStyling >
-            <NavLink to= "/recipeInstructions" state={{url: props.item.url}}><img src={props.item.url}/></NavLink> {/*this allows clicking the picture to pass the props down*/}
-            <p className="ingredients">{props.item.ingredients}</p>
-            <p className="preperation">{props.item.preperation}</p>
+            <NavLink to= "/recipeInstructions" state={{url: props.item.url, ingredients: props.item.ingredients, preperation: props.item.preperation, name: props.item.name}}><img src={props.item.url} onMouseOver={handleHover} onMouseOut={handleUnhover}/></NavLink> {/*this allows clicking the picture to pass the props down*/}
+            {isHovered && (<p>{props.item.name}</p>)}
         </ImgStyling>
     )
 }
--- src/pages/recipeInstructions.jsx
+++ src/pages/recipeInstructions.jsx
@@ -4,9 +4,15 @@ import {useLocation} from 'react-router-dom'
 export default function recipeInstructions(props){
     const location = useLocation();
     const { url } = location.state
+    const { ingredients } = location.state
+    const { preperation } = location.state
+    const { name } = location.state
     return(
         <div>
             <img src={url}/>
+            <p>{name}</p>
+            <p className="ingredients">{ingredients}</p>
+            <p className="preperation">{preperation}</p>
         </div>
     )
 }
