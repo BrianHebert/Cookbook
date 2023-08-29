0c035c2bef452903f7587e92fa46b54dfb045067
BrianHebert <butterflyrbrian40@gmail.com>
2023-08-28 21:59:14 -0500

    the recipes from the api now show ingredients after being clicked

--- src/pages/recipeApiInstructions.jsx
+++ src/pages/recipeApiInstructions.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import {useLocation} from 'react-router-dom'
+import styled from "styled-components";
+
+const RecipeStyling = styled.div`
+    white-space: pre-wrap; /* makes it so \n works as a new line */
+`;
 
 export default function recipeApiInstructions(props){
     const location = useLocation();
     const { recipe } = location.state
+    const name = recipe.label
+    const ingredientArray = recipe.ingredientLines
+    let ingredients= ""
+    console.log(recipe)
     console.log(recipe.label)
+    console.log(ingredientArray[0])
+
+    for (let i = 0; i < ingredientArray.length; i++){
+        ingredients += JSON.stringify(ingredientArray[i])
+    }
+
+
     return(
         <div>
-            HI
+            <RecipeStyling>
+                <h1 className="recipe">{name}</h1>
+                <p>{ingredients.replace(/"/g, '\n')}</p>
+                
+            </RecipeStyling>
         </div>
     )
 }
